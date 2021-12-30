461b85769e2b39220939a5807b5296ac8d67e95d
nabin <nkbhaie2017@gmail.com>
2021-12-30 16:22:38 -0700

    modify nav.js file

--- js/nav.js
+++ js/nav.js
@@ -34,3 +34,60 @@ function updateNavOnLogin() {
   $navLogOut.show();
   $navUserProfile.text(`${currentUser.username}`).show();
 }
+
+// Any thing below is my work:
+// user story
+/*
+function navUserStoryClick(evt){
+    console.debug("navUserStoryClick", evt);
+    hidePageComponents();  // this will hide   $allStoriesList, $loginForm, $signupForm,
+    $allStoriesList.show();
+    $submitForm.show();
+}
+*/
+
+// My addition Every thing below is my addition
+
+/** Show story submit form on clicking story "submit" */
+// when a user clicks on the submit, that is in navbar, show story PART 2B
+
+// function navSubmitStoryClick(evt) {  
+function navSubmitClick(evt) {           // PART 2B, function for submit link in 
+    console.debug("navSubmitClick", evt);
+    hidePageComponents();
+    // $allStoriesList.show();
+    $submitForm.show();
+  }
+  
+  $navSubmitStory.on("click", navSubmitClick);
+  
+  /** Show favorite stories on click on "favorites" */
+  
+  function navFavoritesClick(evt) {
+    console.debug("navFavoritesClick", evt);
+    hidePageComponents();
+    putFavoritesListOnPage();
+  }
+  
+  $body.on("click", "#nav-favorites", navFavoritesClick);
+  
+  /** Show My Stories on clicking "my stories" */
+  
+  function navMyStories(evt) {
+    console.debug("navMyStories", evt);
+    hidePageComponents();
+    putUserStoriesOnPage();
+    $ownStories.show();
+  }
+  
+  $body.on("click", "#nav-my-stories", navMyStories);
+  
+/** Hide everything but profile on click on "profile" */
+
+function navProfileClick(evt) {
+    console.debug("navProfileClick", evt);
+    hidePageComponents();
+    $userProfile.show();
+  }
+  
+  $navUserProfile.on("click", navProfileClick);
